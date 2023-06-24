b5b1801436a5f959fddd6dd6734c1097d9ea0576
Keerthi Vardhan <keerthivardhantekulapelli@gmail.com>
2023-06-24 12:55:49 +0530

    npm command  commit

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'cjs',
+  },
+  plugins: [
+    resolve({
+      extensions: ['.js', '.jsx' , 'css'], // Specify the extensions to be resolved
+    }),
+    commonjs(),
+    postcss(),
+  ],
+};
